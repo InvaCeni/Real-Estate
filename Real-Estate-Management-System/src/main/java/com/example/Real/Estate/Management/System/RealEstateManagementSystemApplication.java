b481293d846c1650ab5d8eb0918b6dff215fdc5e
Inva Ceni <57628673+InvaCeni@users.noreply.github.com>
2025-02-08 10:56:41 +0100

    Created Admin Controller

--- Real-Estate-Management-System/src/main/java/com/example/Real/Estate/Management/System/RealEstateManagementSystemApplication.java
+++ Real-Estate-Management-System/src/main/java/com/example/Real/Estate/Management/System/RealEstateManagementSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealEstateManagementSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RealEstateManagementSystemApplication.class, args);
 	}
 
