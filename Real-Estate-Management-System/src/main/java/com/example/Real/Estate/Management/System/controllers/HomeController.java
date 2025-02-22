1b57bd3d8d50f5291f5ccd3e6890f540041279fb
Inva Ceni <57628673+InvaCeni@users.noreply.github.com>
2025-02-22 15:34:34 +0100

    ThymeLeaf FrontEnd

---
+++ Real-Estate-Management-System/src/main/java/com/example/Real/Estate/Management/System/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.Real.Estate.Management.System.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
